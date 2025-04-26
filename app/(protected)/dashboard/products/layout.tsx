437880f409721fb267db8b3516913ced680463d9
Yen Colon <yenjcc@gmail.com>
2025-04-26 15:42:34 -0400

    Add suspense for URL search params

--- app/(protected)/dashboard/products/layout.tsx
+++ app/(protected)/dashboard/products/layout.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { type ReactNode } from "react";
+import { Suspense, type ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="p-2 sm:p-6">
-      
-      <div className="flex flex-col justify-center items-center gap-5">
-        {children}
-      </div>
+      <Suspense>
+        <div className="flex flex-col justify-center items-center gap-5">
+          {children}
+        </div>
+      </Suspense>
     </div>
   );
 }
