f9db958328198358276001305d6e519112189eb2
Yen Colon <yenjcc@gmail.com>
2025-06-06 15:40:53 -0400

    Add nextjs config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,50 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '2mb',
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "www.esmeraldaenlinea.com/uploads",
+        protocol: "https",
+        pathname: "**",
+        port: ''
+      }
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self' https://api.daily.dev https://api.github.com https://github.com https://www.linkedin.com; frame-src 'self' https://app.daily.dev; object-src 'none'; base-uri 'none'; form-action 'none'",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
